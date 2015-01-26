503476ae745d3a7b04df50f3500b9d5183ddc809
Eric Myhre <eric@flynn.io>
2015-01-26 12:43:28 -0800

    test: Add tests for host manipulation in tests.
    This exercises the APIs for hosts much as some of the scheduler tests were already doing, but in isolation so we can clearly see if anything's going wrong at a basic level before getting into interesting flynn application code.
    
    Signed-off-by: Eric Myhre <eric@flynn.io>

---
+++ test/test_bounce.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	c "github.com/flynn/flynn/Godeps/_workspace/src/github.com/flynn/go-check"
+	"github.com/flynn/flynn/test/cluster/client"
+)
+
+/*
+	Sanity check over our CI system to make sure we can in fact bounce hosts,
+	and that does what we think it does.
+
+	Note that any tests that involve raising or destroying hosts that join the
+	main test bootstrap cluster cannot be run concurrently.
+*/
+type BounceSuite struct {
+	Helper
+}
+
+var _ = c.Suite(&BounceSuite{})
+
+func (BounceSuite) SetUpSuite(t *c.C) {
+	if args.ClusterAPI == "" {
+		t.Skip("cannot boot new hosts")
+	}
+}
+
+func (s *BounceSuite) TestHostUpDown(t *c.C) {
+	cl := &testcluster.Client{
+		C:          httpClient,
+		ClusterAPI: args.ClusterAPI,
+	}
+	hosts, err := cl.AddHosts(testCluster, s.clusterClient(t), 1)
+	t.Assert(err, c.IsNil)
+	t.Assert(hosts, c.HasLen, 1)
+	err = cl.RemoveHosts(s.discoverdClient(t), hosts)
+	t.Assert(err, c.IsNil)
+}
+
+func (s *BounceSuite) TestBounceHostProcess(t *c.C) {
+	// TODO
+}
+
+func (s *BounceSuite) TestBounceHostVM(t *c.C) {
+	// TODO
+}
