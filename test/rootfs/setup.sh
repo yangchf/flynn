2d85e4a13ddc9c812cc4b81ab3d70a03ade861df
Jonathan Rudenberg <jonathan@titanous.com>
2015-01-28 10:57:21 -0800

    test/rootfs: Install kernel 3.16.0
    The default trusty kernel is 3.13, but the backported “LTS Enablement”
    package is already available and will be the default in 14.04.2
    
    Signed-off-by: Jonathan Rudenberg <jonathan@titanous.com>

--- test/rootfs/setup.sh
+++ test/rootfs/setup.sh
@@ -66,7 +66,7 @@ apt-get dist-upgrade \
   -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confold"
-apt-get install linux-generic-lts-trusty \
+apt-get install --install-recommends linux-generic-lts-utopic \
   -y \
   -o Dpkg::Options::="--force-confdef" \
   -o Dpkg::Options::="--force-confold"
